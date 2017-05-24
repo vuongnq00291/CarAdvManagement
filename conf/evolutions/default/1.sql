1c56bff1b69838460df752a6769b63ef50a1dda4
vuong <vuongnq00291@gmail.com>
2017-05-24 10:26:20 +0800

    forgot evolutions

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,16 @@
+
+# --- !Ups
+
+CREATE TABLE CarAdv (
+  id         INTEGER      NOT NULL PRIMARY KEY AUTOINCREMENT,
+  title      VARCHAR(255) NOT NULL,
+  fual       VARCHAR(255) NOT NULL,
+  price      INTEGER NOT NULL,
+  newone        INTEGER NOT NULL,
+  mileage    INTEGER,
+  first_registration LONG
+);
+
+# --- !Downs
+
+DROP TABLE CarAdv;
